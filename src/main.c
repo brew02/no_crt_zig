c6d8cb4b5091a7ca1a426f121d2dc56d4ed64133
brew02 <brodiea2005@gmail.com>
2025-05-18 02:18:32 -0600

    Remove unnecessary symbols

--- src/main.c
+++ src/main.c
@@ -1,15 +1,5 @@
 #include <windows.h>
 
-int main() {
-    MessageBoxA(0, "Hello", 0, 0);
-    return 0;
-}
-
-int __main() {
-    MessageBoxA(0, "Hello", 0, 0);
-    return 0;
-}
-
 int wWinMainCRTStartup() {
     MessageBoxA(0, "Hello", 0, 0);
     return 0;
